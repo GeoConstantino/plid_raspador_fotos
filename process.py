45343ebcd80d4e0e66d383171290f19634df74c8
Geovani Constantino <geovani.cpm@gmail.com>
2019-06-13 19:05:02 -0300

    Adicionado feature de leitura de csv

--- process.py
+++ process.py
@@ -1,8 +1,12 @@
 import base64
+import os
+
+import pandas as pd
 
 from zeep import Client
-
 from decouple import config
+
+import ipdb
 
 # configurações de acesso
 CNPJ = config('CNPJ')
@@ -11,18 +15,23 @@ PERFIL = config('PERFIL')
 CPF = config('CPF')
 
 # local para fotos
-PATH = "fotos"
+PATH = "out"
 
 
 def manda_consulta(rg, conn):
     #conn = Client(URL_CONSULTA_RG)
+    print('sending {}'.format(rg))
     conn.service.consultarRG(CNPJ,CHAVE,PERFIL,rg, rg.zfill(10),CPF)
 
 
 def pega_resultado(rg, ress):
     res = ress.service.BuscarProcessados(CNPJ,CHAVE,PERFIL,rg)
-    res = res[0]
-    return (res.fotoCivil)
+    try:
+        res1 = res[0].fotoCivil
+        res2 = res1.string[0]
+    except:
+        return None
+    return (res2)
 
 def conectar_busca():
     return (Client("http://10.200.96.170:8080/servico.asmx?wsdl"))
@@ -32,30 +41,39 @@ def conectar_resultado():
 
 
 def salva_foto(foto, rg):
-    try:
-        with open('{path}/{rg}.jpg'.format(rg=rg, path=PATH), 'wb') as fobj:
-            fobj.write(base64.b64decode(str.encode(foto)))
-        print("foto salva: {}".format)
-    except:
-        print("erro ao salvar o arquivo do rg: {}".format(rg)) 
+    with open('{path}/{rg}.jpg'.format(rg=rg, path=PATH), 'wb') as fobj:
+        fobj.write(base64.b64decode(str.encode(foto)))
+    
+   #print('foto salva:' + rg)
+
+    
+
+def lista_rgs(file):
+    df = pd.read_csv(file, encoding='latin1')
+    df = df.loc[:,['VTMA_RG','SNCA_VTMA_DK']]
+    df = df.values.tolist()
+
+    return df
 
 if __name__=="__main__":
 
-    #procesa csv
-    # 
-
     conn = conectar_busca()
-
     ress = conectar_resultado()
 
-    lista_rg = ['211367230','98255060']
+    for root,dirs,files in os.walk("in"):
+        for file in files:
+            if file.endswith(".csv"):
+                file_rgs = lista_rgs(file)
+                
+                for rg, ident in file_rgs:
+                    manda_consulta(rg, conn)
 
-    for rg in lista_rg:
-        manda_consulta(rg, conn)
-
-    for rg in lista_rg:
-        foto = pega_resultado(rg, ress)
-        if foto is not None:
-            salva_foto(foto.string[0], rg)
-        else:
-            print("{} foto não localizada.".format(rg))
+                for rg, ident in file_rgs:
+                    try:
+                        foto = pega_resultado(rg, ress)
+                    except TypeError as error:
+                        print("Não encontrado {errorg}, {tipoerro}".format(errorg=rg, tipoerro=error))
+                    if foto is not None:
+                        salva_foto(foto, ident)
+                    else:
+                        print("{} foto não localizada.".format(rg))
--- test_process.py
+++ test_process.py
@@ -1,4 +1,10 @@
 import pytest
+import os
+import pandas as pd
 
-from process import None
+from process import lista_rgs
+
+def test_lista_rgs():
+    file_csv = os.getcwd()+"/in/test_lista_rgs.csv"
+    assert lista_rgs(file_csv) == [[130015340,6],[98255060,10]]
 
